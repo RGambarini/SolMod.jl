40fdad53d20fbcd72500243691a8d86e2fbba303
RGambarini <robertogambarini11@gmail.com>
2022-05-13 14:49:45 +0100

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test, SolubilityModeling # This load both the test suite and our Package
 
-2*3
+out = 2*3
 
 @test out == 6               # This is the actual test condition. You can add as many tests as you wish.
