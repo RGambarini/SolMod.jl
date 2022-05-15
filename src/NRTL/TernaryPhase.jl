34e5672e89fbbc1013ac6a9932b5199c767535b7
RGambarini <robertogambarini11@gmail.com>
2022-05-16 00:43:50 +0100

    @JuliaRegistrator register
    Release notes:
    
    added TernaryPhase.jl

--- src/NRTL/TernaryPhase.jl
+++ src/NRTL/TernaryPhase.jl
@@ -8,34 +8,53 @@ function NRTL_ternaryPhase(params::Dict, solvent::String, Tx; x_step::Float64 = 
     fusΔH_rac = params["Solute"][3]; Tm_rac = params["Solute"][4]
     ⍺ = params[solvent]["⍺"]; g = params[solvent]["g"]
     J = 1:length(⍺[1, :]); K = 1:length(⍺[1, :]); M = 1:length(⍺[1, :])
-  
-  function NRTLr(xi, T)
-      𝜏 = g/(T*R)
-      G = (ℯ*ones(size(⍺))).^(-1*⍺.*𝜏)
-      nγi = exp(sum(xi[j]*𝜏[j, 1]*G[j, 1] for j in J)/sum(xi[k]*G[k, 1] for k in K)+sum(xi[j]*G[1, j]/sum(xi[k]*G[k, j] for k in K)*(𝜏[1, j]-(sum(xi[m]*𝜏[m, j]*G[m, j] for m in M)/sum(xi[k]*G[k, j] for k in K))) for j in J))
-      nzi = log((xi[1])^2*(nγi)^2)+log(4)-(fusΔH_rac/R)*(1/T-1/Tm_rac)
-      nzi^2
+
+  function NRTLei(xi, T)
+      
+    𝜏 = g/(T*R)
+    G = (ℯ*ones(size(⍺))).^(-1*⍺.*𝜏)
+    nlnγi = sum(xi[j]*𝜏[j, 1]*G[j, 1] for j in J)/sum(xi[k]*G[k, 1] for k in K)+sum(xi[j]*G[1, j]/sum(xi[k]*G[k, j] for k in K)*(𝜏[1, j]-(sum(xi[m]*𝜏[m, j]*G[m, j] for m in M)/sum(xi[k]*G[k, j] for k in K))) for j in J)
+    nzi = log(xi[1])+nlnγi-(fusΔH/R)*(1/T-1/Tm)
+    nzi^2
 
   end
 
-  function NRTLe(xi, T)
-      𝜏 = g/(T*R)
-      G = (ℯ*ones(size(⍺))).^(-1*⍺.*𝜏)
-      nlnγi = sum(xi[j]*𝜏[j, 1]*G[j, 1] for j in J)/sum(xi[k]*G[k, 1] for k in K)+sum(xi[j]*G[1, j]/sum(xi[k]*G[k, j] for k in K)*(𝜏[1, j]-(sum(xi[m]*𝜏[m, j]*G[m, j] for m in M)/sum(xi[k]*G[k, j] for k in K))) for j in J)
-      nzi = log(xi[1])+nlnγi-(fusΔH/R)*(1/T-1/Tm)
-      nzi^2
-
-    end
-
-  function r(xi)
-
-      NRTLr(xi, Tx)
+  function NRTLej(xi, T)
+      
+    𝜏 = g/(T*R)
+    G = (ℯ*ones(size(⍺))).^(-1*⍺.*𝜏)
+    nlnγi = sum(xi[j]*𝜏[j, 2]*G[j, 2] for j in J)/sum(xi[k]*G[k, 2] for k in K)+sum(xi[j]*G[2, j]/sum(xi[k]*G[k, j] for k in K)*(𝜏[2, j]-(sum(xi[m]*𝜏[m, j]*G[m, j] for m in M)/sum(xi[k]*G[k, j] for k in K))) for j in J)
+    nzi = log(xi[2])+nlnγi-(fusΔH/R)*(1/T-1/Tm)
+    nzi^2
 
   end
 
-  function e(xi)
+  function NRTLr(xi, xj, T)
 
-      NRTLe(xi, Tx)
+    𝜏 = g/(T*R)
+    G = (ℯ*ones(size(⍺))).^(-1*⍺.*𝜏)
+    lnγi = sum(xi[j]*𝜏[j, 1]*G[j, 1] for j in J)/sum(xi[k]*G[k, 1] for k in K)+sum(xi[j]*G[1, j]/sum(xi[k]*G[k, j] for k in K)*(𝜏[1, j]-(sum(xi[m]*𝜏[m, j]*G[m, j] for m in M)/sum(xi[k]*G[k, j] for k in K))) for j in J)
+    lnγj = sum(xi[j]*𝜏[j, 2]*G[j, 2] for j in J)/sum(xi[k]*G[k, 2] for k in K)+sum(xi[j]*G[2, j]/sum(xi[k]*G[k, j] for k in K)*(𝜏[2, j]-(sum(xi[m]*𝜏[m, j]*G[m, j] for m in M)/sum(xi[k]*G[k, j] for k in K))) for j in J)
+    nzi = (2*fusΔH_rac/R)*(1/Tm_rac-1/T) - (log(4) + lnγi + lnγj + log(xi[1]) + log(xj[2]))
+    nzi^2
+
+  end
+
+  function ei(xi)
+
+    NRTLei(xi, Tx)
+
+  end
+
+  function ej(xi)
+
+    NRTLej(xi, Tx)
+
+  end
+
+  function r(xi, xj)
+
+    NRTLr(xi, xj, Tx)
 
   end
 
@@ -43,17 +62,30 @@ function NRTL_ternaryPhase(params::Dict, solvent::String, Tx; x_step::Float64 = 
 
       for j in 0.001:x_step:0.999
 
-      for k in 0.001:x_step:0.999
+        for k in 0.001:x_step:0.999
 
-          if i + j + k == 1 && i/j > 1
+            if i + j + k == 1
 
-          if round(e([i, j, k]), digits = 3) == 0
+              if round(ei([i, j, k]), digits = 3) == 0 && i/j > 1
 
-              append!(x_1v, i); append!(x_2v, j); append!(x_3v, k)
+                append!(x_1v, i); append!(x_2v, j); append!(x_3v, k)
 
-          end
-          end
-      end
+              end
+
+              if round(ej([i, j, k]), digits = 3) == 0 && i/j < 1
+
+                append!(x_1v, i); append!(x_2v, j); append!(x_3v, k)
+
+              end
+
+              if round(r([i, j, k], [i, j, k]), digits = 3) == 0
+
+                append!(x_1v, i); append!(x_2v, j); append!(x_3v, k)
+
+              end
+
+            end
+        end
       end
   end
 
