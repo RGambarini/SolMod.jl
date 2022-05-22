d0472c17a73caac8e2233d61787b79c8337b1e41
RGambarini <robertogambarini11@gmail.com>
2022-05-22 16:40:08 +0100

    @JuliaRegistrator register
    Release:
    
    Added a test for TPD

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using Test, SolubilityModeling # This load both the test suite and our Package
 
-out = 2*3
+params = Dict()
 
-@test out == 6               # This is the actual test condition. You can add as many tests as you wish.
+params["Solute"] = [24500 404.65 25600 387.75]
+
+params["Diethyl Tartrate"] = Dict()
+params["Diethyl Tartrate"]["⍺"] = [0 0.979748 0.40104 ; 0.979748 0 0.40104; 0.40104 0.40104 0 ]
+params["Diethyl Tartrate"]["g"] = [0 216631 25269 ; 141879 0 25269; -3235.6 -3235.6 0 ]
+
+out = NRTL_ternaryPhase(params, "Diethyl Tartrate", [298.0])[298.0][1000]
+
+@test out == 0.115               # This is the actual test condition. You can add as many tests as you wish.
