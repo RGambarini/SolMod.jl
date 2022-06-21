b85469980a135c73e89e8446ff993578905fafd7
RGambarini <robertogambarini11@gmail.com>
2022-06-21 12:52:35 +0100

    Fixed failing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,8 @@ params["Diethyl Tartrate"] = Dict()
 params["Diethyl Tartrate"]["⍺"] = [0 0.979748 0.40104 ; 0.979748 0 0.40104; 0.40104 0.40104 0 ]
 params["Diethyl Tartrate"]["g"] = [0 216631 25269; 141879 0 25269; -3235.6 -3235.6 0 ]
 
-lnγi() = NRTL_activityCoefficient(params, "Diethyl Tartrate", 298)
-lnγj() = NRTL_activityCoefficient(params, "Diethyl Tartrate", 298, e = true)
-
-out = ternaryPhase(params, "Diethyl Tartrate", lnγi, lnγj, 298.0)[1000]
+γi() = NRTL_activityCoefficient(params, "Diethyl Tartrate", 298)
+γj() = NRTL_activityCoefficient(params, "Diethyl Tartrate", 298, e = true)
+out = ternaryPhase(params, γi, γj, 298)[1000]
 
 @test out == 0.11               # This is the actual test condition. You can add as many tests as you wish.
