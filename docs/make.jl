ca969f2f89021fe14edb70d1df48acdff53d1711
RGambarini <robertogambarini11@gmail.com>
2022-06-26 09:34:57 +0100

    Added pages to website

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,23 @@ makedocs(sitename="SolMod",
 ),
     author = "Roberto Gambarini.",
     pages = [
-            "Home" => "index.md"
-         ],
-)
+            "Home" => "index.md",
+         "Background" => "background.md",
+         "Basic Usage" => "basic_usage.md",
+         "Notebook Examples" => "notebook_examples.md",
+         "To-do list" => "to-do_list.md",
+         
+         "Available Models" => [
+         "Empirical Models" => Any[
+            "Wilson"=>"AvailableModels/Wilson.md",
+            "NRTL"=>"AvailableModels/NRTL.md",
+            "UNIQUAC"=>"AvailableModels/UNIQUAC.md"]
+         "Semi-Empirical Models" => Any[
+            "NRTL-SAC"=>"AvailableModels/NRTL-SAC.md",
+            "UNIQUAC-SAC"=>"AvailableModels/UNIQUAC-SAC.md"]
+         "Predictive Models" => Any[
+            "PC-SAFT"=>"AvailableModels/PC-SAFT.md"]
+         ]])
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
